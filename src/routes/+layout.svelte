bc1f0b796d469b1291087a8c63e8c6264a11a1cc
rajeevrajchal <rajeevrajchal12@gmail.com>
2026-03-07 12:40:49 +0100

    added debug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '@styles/css/global.css';
 
 	import favicon from '@assets/favicon.svg';
+	import Debug from '@modules/tweak-pane/debug.svelte';
 
 	let { children } = $props();
 </script>
@@ -11,4 +12,7 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<main class="app-content">
+	{@render children()}
+</main>
+<Debug />
